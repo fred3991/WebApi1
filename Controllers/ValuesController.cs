8e937f15be41bbcfd0f29a615fe40a3a5c7067ab
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2019-08-15 14:34:43 +0700

    Fak

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using WebApi1.Models;
+using System.Data;
+
+namespace WebApi1.Controllers
+{
+
+    public class ValuesController : ApiController
+    {
+        BookContext db = new BookContext();
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return db.Books;
+        }
+
+        public Book GetBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            return book;
+        }
+
+        [HttpPost]
+        public void CreateBook([FromBody]Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+        }
+
+        [HttpPut]
+        public void EditBook(int id, [FromBody]Book book)
+        {
+            if (id == book.Id)
+            {
+                db.Entry(book).State = EntityState.Modified;
+
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            if (book != null)
+            {
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
